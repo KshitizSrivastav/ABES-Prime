60cd89ec939964ef87378439a3236b105d534510
KshitizSrivastav <kshitizsr02@gmail.com>
2025-02-14 16:53:50 +0530

    Js Day1

---
+++ Javascript/Day1/day1.js
@@ -0,0 +1,10 @@
+let a=20;
+let b=40;
+let sum=a+b
+document.writeln("Sum of two numbers is "+sum);
+if (a>b){
+    document.writeln("A is greater than B");
+}
+else{
+    document.writeln("B is greater than A");
+}
